414b0832f3ee70ff5235086ccfe1c517979915e3
Pit_killer2.1 <brad.alvarez@epn.edu.ec>
2017-05-22 17:56:18 -0500

    mi funcion "nose si este bien :v"

--- Taller1.py
+++ Taller1.py
@@ -6,3 +6,12 @@ def DanielCasagallo():
 tk = Tk()
 btn = Button(tk, text = "Daniel", command = DanielCasagallo())
 btn.pack()
+
+def SebastianAlvarez():
+    print 'Sebastian Alvarez  :v'
+
+tk = tk()
+btn = Button(tk, text = "Sebas", command = SebastianAlvarez())
+btn.pack()
+
+
