646545c35ab740dc67490880d31ffd1af0438a2c
22B22-PC\22B <jona_maiza@hotmail.es>
2017-05-23 13:47:15 -0500

    Agrege mi función

--- Taller1.py
+++ Taller1.py
@@ -2,7 +2,17 @@ from tkinter import *
 
 def DanielCasagallo():
     print 'Daniel Casagallo'
-
 tk = Tk()
 btn = Button(tk, text = "Daniel", command = DanielCasagallo())
 btn.pack()
+
+def JavierMaiza():
+    print 'Jonathan Javier Maiza'
+
+tk=Tk()
+btn= Button (tk, text='Javier', command= JavierMaiza())
+btn.pack()
+btn1=Button(tk, text='Daniel', command= DanielCasagallo())
+btn1.pack()
+
+
